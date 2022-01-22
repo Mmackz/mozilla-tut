94f72f23d739e45ed357ff58e674b86a4cc85de8
Mmackz <matthewmackenz@gmail.com>
2022-01-22 11:00:54 -0800

    author form

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const async = require("async");
 const Author = require("../models/author");
 const Book = require("../models/book");
@@ -41,13 +42,54 @@ exports.author_detail = (req, res, next) => {
 
 // Display Author create form on GET
 exports.author_create_get = (req, res) => {
-   res.send("NOT IMPLEMENTED: Author create GET");
+   res.render("author_form", { title: "Create Author" });
 };
 
 // Handle Author create on POST
-exports.author_create_post = (req, res) => {
-   res.send("NOT IMPLEMENTED: Author create POST");
-};
+exports.author_create_post = [
+   body("first_name")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("First name must be specified.")
+      .isAlphanumeric()
+      .withMessage("First name has non-alphanumeric characters."),
+   body("family_name")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("Family name must be specified.")
+      .isAlphanumeric()
+      .withMessage("Family name has non-alphanumeric characters."),
+   body("date_of_birth", "Invalid date of birth")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+   body("date_of_death", "Invalid date of death")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+
+   (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+         res.render("author_form", {
+            title: "Create Author",
+            author: req.body,
+            errors: errors.array()
+         });
+      }
+
+      const { first_name, family_name, date_of_birth, date_of_death } = req.body
+
+      const author = new Author({
+         ...req.body
+      })
+
+      console.log(author)
+   }
+];
 
 // Display Author delete form on GET
 exports.author_delete_get = (req, res) => {
