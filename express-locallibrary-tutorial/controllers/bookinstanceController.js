058ae893ea24e47fd4c8aab39869226f7b5f952b
Mmackz <matthewmackenz@gmail.com>
2022-01-23 12:23:38 -0800

    book instance delete

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -99,12 +99,15 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET
 exports.bookinstance_delete_get = (req, res) => {
-   res.send("NOT IMPLEMENTED: BookInstance delete GET");
+   res.render("bookinstance_delete", { title: "Delete Book Instance", id: req.params.id });
 };
 
 // Handle BookInstance delete on POST
-exports.bookinstance_delete_post = (req, res) => {
-   res.send("NOT IMPLEMENTED: BookInstance delete POST");
+exports.bookinstance_delete_post = (req, res, next) => {
+   BookInstance.findByIdAndRemove(req.body.bookinstance_id, err => {
+      if (err) return next(err);
+      res.redirect("/catalog/bookinstances")
+   })
 };
 
 // Display BookInstance update form on GET
